621f0867f6edee05d99f6c4548f86e6dceab915c
Gabriele Ribichini <gabriele.ribichini@gmail.com>
2015-05-05 00:16:57 +0200

    network size adjusted to 30 Nodes and 40 Slots

--- libraries/grhLib/grhSoulissCommon.h
+++ libraries/grhLib/grhSoulissCommon.h
@@ -10,7 +10,7 @@
 // Define the souliss network dimensions
 #define NODESIZE_INSKETCH
 #define MaCaco_NODES			30		// MaCaco_NODES < 255
-#define MaCaco_SLOT				35 		// MaCaco_SLOT < VNET_MAX_PAYLOAD - MaCaco_HEADER and MaCaco_SLOT  < 255 / 3
+#define MaCaco_SLOT				40 		// MaCaco_SLOT < VNET_MAX_PAYLOAD - MaCaco_HEADER and MaCaco_SLOT  < 255 / 3
 
 
 #ifdef DEBUG
