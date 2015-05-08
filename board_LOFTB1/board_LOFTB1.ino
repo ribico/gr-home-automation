e64b573b8412da7cd1a5fe5906b8ff7c84c9db1e
Gabriele Ribichini <gabriele.ribichini@gmail.com>
2015-05-08 23:13:20 +0200

    Modified GW sketch with T1A to free some slots

--- board_LOFTB1/board_LOFTB1.ino
+++ board_LOFTB1/board_LOFTB1.ino
@@ -22,8 +22,8 @@ MEGA with Ethernet only acting as GATEWAY
 #include "NTC.h"
 
 
-#define LIGHT_LOFT_1            1                       
-#define LIGHT_LOFT_2            2
+#define LIGHT_LOFT_1            		1                       
+#define LIGHT_LOFT_2            		2
 #define LIGHT_TERRACE_1                 3
 #define LIGHT_TERRACE_2                 4
 #define LIGHT_TERRACE_3                 5
@@ -32,34 +32,39 @@ MEGA with Ethernet only acting as GATEWAY
 #define TEMP_BOILER_ACS                 7
 #define TEMP_BOILER_ACS_H               8
 #define TEMP_BOILER_HEATING             9
-#define TEMP_BOILER_HEATING_H   10
+#define TEMP_BOILER_HEATING_H   		10
 #define TEMP_BOILER_BOTTOM              11
-#define TEMP_BOILER_BOTTOM_H    12
+#define TEMP_BOILER_BOTTOM_H    		12
 
-#define HEATPUMP_AUTO                           13
+#define HEATPUMP_AUTO          			13
 #define HEATPUMP_REMOTE_SWITCH          14
 #define HEATPUMP_CIRCULATION_PUMP       15
 #define HEATPUMP_ACS_REQUEST            16
-#define HEATPUMP_COOL                           17      
+#define HEATPUMP_COOL                	17      
 
-#define TEMP_HEATING_FLOW                       18      // floor heating system water temp
-#define TEMP_HEATING_FLOW_H                     19
-#define TEMP_HEATING_RETURN                     20      // floor heating system water return temp
-#define TEMP_HEATING_RETURN_H           21
+#define TEMP_HEATING_FLOW 			18      // floor heating system water temp
+#define TEMP_HEATING_FLOW_H 		19
+#define TEMP_HEATING_RETURN  		20      // floor heating system water return temp
+#define TEMP_HEATING_RETURN_H 		21
 
-#define HEATING_KITCHEN         22
-#define HEATING_BATH2           23
-#define HEATING_BED3            24
-#define HEATING_LIVING          25
-#define HEATING_BED2            26
-#define HEATING_BATH1           27
-#define HEATING_BED1            28
-#define HEATING_LOFT            29
+#define HVAC_ZONES					22
+#	define HVAC_MASK_BED1			0x01
+#	define HVAC_MASK_BATH1			0x02
+#	define HVAC_MASK_BED2			0x04
+#	define HVAC_MASK_LIVING			0x08
+#	define HVAC_MASK_BED3			0x10
+#	define HVAC_MASK_BATH2			0x20
+#	define HVAC_MASK_KITCHEN		0x40
+#	define HVAC_MASK_LOFT			0x80
 
-#define HEATING_MIX_VALVE_SWITCH	30
-#define HEATING_MIX_VALVE_DIRECTION	31
+#define HEATING_MIX_VALVE 						23
+#	define HEATING_MIX_VALVE_SWITCH_MASK		0x01
+#	define HEATING_MIX_VALVE_DIRECTION_MASK		0x02
 
-#define TEMP_BOILER_ACS_PIN             0       // analog pin used
+
+
+
+#define TEMP_BOILER_ACS_PIN 	0       // analog pin used
 #define TEMP_BOILER_HEATING_PIN 1       // analog pin used
 #define TEMP_BOILER_BOTTOM_PIN  2       // analog pin used
 #define TEMP_HEATING_FLOW_PIN   3       // analog pin used
@@ -167,26 +172,21 @@ inline void DefineTypicals()
 	Set_SimpleLight(LIGHT_TERRACE_2);
 	Set_SimpleLight(LIGHT_TERRACE_3);
 	Set_SimpleLight(LIGHT_TOILET);
+
 	Souliss_SetT11(memory_map, HEATPUMP_AUTO);
 	Souliss_SetT11(memory_map, HEATPUMP_REMOTE_SWITCH);
 	Souliss_SetT11(memory_map, HEATPUMP_CIRCULATION_PUMP);
 	Souliss_SetT11(memory_map, HEATPUMP_ACS_REQUEST);
 	Souliss_SetT11(memory_map, HEATPUMP_COOL);
+	
 	Set_Temperature(TEMP_BOILER_ACS);
 	Set_Temperature(TEMP_BOILER_HEATING);
 	Set_Temperature(TEMP_BOILER_BOTTOM);
 	Set_Temperature(TEMP_HEATING_FLOW);
 	Set_Temperature(TEMP_HEATING_RETURN);
-	Souliss_SetT11(memory_map, HEATING_KITCHEN);
-	Souliss_SetT11(memory_map, HEATING_BATH2);
-	Souliss_SetT11(memory_map, HEATING_BED3);
-	Souliss_SetT11(memory_map, HEATING_LIVING);
-	Souliss_SetT11(memory_map, HEATING_BED2);
-	Souliss_SetT11(memory_map, HEATING_BATH1);
-	Souliss_SetT11(memory_map, HEATING_BED1);
-	Souliss_SetT11(memory_map, HEATING_LOFT);
-	Souliss_SetT11(memory_map, HEATING_MIX_VALVE_SWITCH);
-	Souliss_SetT11(memory_map, HEATING_MIX_VALVE_DIRECTION);
+
+	Souliss_SetT1A(memory_map, HVAC_ZONES);
+	Souliss_SetT1A(memory_map, HEATING_MIX_VALVE);
 }
 
 inline void ReadInputs()
@@ -197,6 +197,7 @@ inline void ReadInputs()
 	Souliss_LowDigIn(LIGHT_TERRACE_2_PIN_IN, Souliss_T1n_ToggleCmd, memory_map, LIGHT_TERRACE_2, true);
 	Souliss_LowDigIn(LIGHT_TERRACE_3_PIN_IN, Souliss_T1n_ToggleCmd, memory_map, LIGHT_TERRACE_3, true);
 	Souliss_LowDigIn(LIGHT_TOILET_PIN_IN, Souliss_T1n_ToggleCmd, memory_map, LIGHT_TOILET, true);
+
 }
 
 inline void ProcessLogics()
@@ -207,26 +208,22 @@ inline void ProcessLogics()
 	Logic_SimpleLight(LIGHT_TERRACE_2);
 	Logic_SimpleLight(LIGHT_TERRACE_3);
 	Logic_SimpleLight(LIGHT_TOILET);
+
 	Souliss_Logic_T11(memory_map, HEATPUMP_AUTO, &data_changed);
 	Souliss_Logic_T11(memory_map, HEATPUMP_REMOTE_SWITCH, &data_changed);
 	Souliss_Logic_T11(memory_map, HEATPUMP_CIRCULATION_PUMP, &data_changed);
 	Souliss_Logic_T11(memory_map, HEATPUMP_ACS_REQUEST, &data_changed);
 	Souliss_Logic_T11(memory_map, HEATPUMP_COOL, &data_changed);
+
 	Logic_Temperature(TEMP_BOILER_ACS);
 	Logic_Temperature(TEMP_BOILER_HEATING);
 	Logic_Temperature(TEMP_BOILER_BOTTOM);
 	Logic_Temperature(TEMP_HEATING_FLOW);
 	Logic_Temperature(TEMP_HEATING_RETURN);
-	Souliss_Logic_T11(memory_map, HEATING_KITCHEN, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_BATH2, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_BED3, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_LIVING, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_BED2, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_BATH1, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_BED1, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_LOFT, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_MIX_VALVE_SWITCH, &data_changed);
-	Souliss_Logic_T11(memory_map, HEATING_MIX_VALVE_DIRECTION, &data_changed);
+
+	Souliss_Logic_T1A(memory_map, HVAC_ZONES, &data_changed);
+	Souliss_Logic_T1A(memory_map, HEATING_MIX_VALVE, &data_changed);
+
 }
 
 inline void SetOutputs()
@@ -241,16 +238,22 @@ inline void SetOutputs()
 	LowDigOut(HEATPUMP_CIRCULATION_PUMP_PIN, Souliss_T1n_Coil, HEATPUMP_CIRCULATION_PUMP);
 	LowDigOut(HEATPUMP_ACS_REQUEST_PIN, Souliss_T1n_Coil, HEATPUMP_ACS_REQUEST);
 	LowDigOut(HEATPUMP_COOL_PIN, Souliss_T1n_Coil, HEATPUMP_COOL);
-	LowDigOut(ZONE_SWITCH_KITCHEN_PIN, Souliss_T1n_Coil, HEATING_KITCHEN);
-	LowDigOut(ZONE_SWITCH_BATH_2_PIN, Souliss_T1n_Coil, HEATING_BATH2);
-	LowDigOut(ZONE_SWITCH_BED_3_PIN, Souliss_T1n_Coil, HEATING_BED3);
-	LowDigOut(ZONE_SWITCH_LIVING_PIN, Souliss_T1n_Coil, HEATING_LIVING);
-	LowDigOut(ZONE_SWITCH_BED_2_PIN, Souliss_T1n_Coil, HEATING_BED2);
-	LowDigOut(ZONE_SWITCH_BATH_1_PIN, Souliss_T1n_Coil, HEATING_BATH1);
-	LowDigOut(ZONE_SWITCH_BED_1_PIN, Souliss_T1n_Coil, HEATING_BED1);
-	LowDigOut(ZONE_SWITCH_LOFT_PIN, Souliss_T1n_Coil, HEATING_LOFT);
-	LowDigOut(HEATING_MIX_VALVE_SWITCH_PIN, Souliss_T1n_Coil, HEATING_MIX_VALVE_SWITCH);
-	LowDigOut(HEATING_MIX_VALVE_DIRECTION_PIN, Souliss_T1n_Coil, HEATING_MIX_VALVE_DIRECTION);
+
+	// HVAC zone valves
+	digitalWrite(ZONE_SWITCH_KITCHEN_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_KITCHEN)	);
+	digitalWrite(ZONE_SWITCH_BATH_2_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_BATH2) 	);
+	digitalWrite(ZONE_SWITCH_BED_3_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_BED3) 	);
+	digitalWrite(ZONE_SWITCH_LIVING_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_LIVING) 	);
+	digitalWrite(ZONE_SWITCH_BED_2_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_BED2) 	);
+	digitalWrite(ZONE_SWITCH_BATH_1_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_BATH1) 	);
+	digitalWrite(ZONE_SWITCH_BED_1_PIN, 	!(mOutput(HVAC_ZONES) & HVAC_MASK_BED1)		);
+	digitalWrite(ZONE_SWITCH_LOFT_PIN, 		!(mOutput(HVAC_ZONES) & HVAC_MASK_LOFT)		);
+
+	// heating mix valve on/off and direction
+	digitalWrite(HEATING_MIX_VALVE_SWITCH_PIN, 		
+		!(mOutput(HEATING_MIX_VALVE) & HEATING_MIX_VALVE_SWITCH_MASK) );
+	digitalWrite(HEATING_MIX_VALVE_DIRECTION_PIN, 
+		!(mOutput(HEATING_MIX_VALVE) & HEATING_MIX_VALVE_DIRECTION_MASK) );
 }
 
 inline void ProcessTimers()
@@ -302,47 +305,33 @@ inline void ProcessTimers()
 			}
 	}
 
-	if( (mOutput(HEATING_KITCHEN)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_BATH2)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_BED3)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_LIVING)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_BED2)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_BATH1)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_BED1)==Souliss_T1n_Coil) ||
-		(mOutput(HEATING_LOFT)==Souliss_T1n_Coil) )
+	// adjust heating mix valve position in order to keep the SETPOINT_HEATING flow temperature
+	if( mOutput(HVAC_ZONES) && mOutput(HEATPUMP_COOL) == Souliss_T1n_OffCoil )
 	{
+		// at least one zone is open
+		// and the heatpump is not set to COOL mode
+
 		if (temperature_heating_flow > SETPOINT_HEATING + SETPOINT_DEADBAND)
 		{
-			mInput(HEATING_MIX_VALVE_SWITCH) = Souliss_T1n_OnCmd;
-			mInput(HEATING_MIX_VALVE_DIRECTION) = Souliss_T1n_OffCmd;
-		}
+			// mix valve on, direction relay off
+			mInput(HEATING_MIX_VALVE) = HEATING_MIX_VALVE_SWITCH_MASK; 
+		}	
 		else if (temperature_heating_flow < SETPOINT_HEATING - SETPOINT_DEADBAND)
 		{
-			mInput(HEATING_MIX_VALVE_SWITCH) = Souliss_T1n_OnCmd;
-			mInput(HEATING_MIX_VALVE_DIRECTION) = Souliss_T1n_OnCmd;
+			// mix valve on, direction relay on
+			mInput(HEATING_MIX_VALVE) = HEATING_MIX_VALVE_SWITCH_MASK | HEATING_MIX_VALVE_DIRECTION_MASK;
 		}
 		else
 		{
-			mInput(HEATING_MIX_VALVE_SWITCH) = Souliss_T1n_OffCmd;
-			mInput(HEATING_MIX_VALVE_DIRECTION) = Souliss_T1n_OffCmd;
+			// mix valve off (hold the position)
+			mInput(HEATING_MIX_VALVE) = 0x0;
 		}		
 	}
 	else
 	{
-		mInput(HEATING_MIX_VALVE_SWITCH) = Souliss_T1n_OffCmd;
-		mInput(HEATING_MIX_VALVE_DIRECTION) = Souliss_T1n_OffCmd;
+		// heating is off, keep the mix valve off.
+		mInput(HEATING_MIX_VALVE) = 0x0;
 	}
-
-	Souliss_T11_Timer(memory_map, HEATING_KITCHEN);
-	Souliss_T11_Timer(memory_map, HEATING_BATH2);
-	Souliss_T11_Timer(memory_map, HEATING_BED3);
-	Souliss_T11_Timer(memory_map, HEATING_LIVING);
-	Souliss_T11_Timer(memory_map, HEATING_BED2);
-	Souliss_T11_Timer(memory_map, HEATING_BATH1);
-	Souliss_T11_Timer(memory_map, HEATING_BED1);
-	Souliss_T11_Timer(memory_map, HEATING_LOFT);
-	Souliss_T11_Timer(memory_map, HEATING_MIX_VALVE_SWITCH);
-	Souliss_T11_Timer(memory_map, HEATING_MIX_VALVE_DIRECTION);
 
 }
 
