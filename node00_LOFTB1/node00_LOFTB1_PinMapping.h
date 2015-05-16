90ebfceaefc8bd187dac22137db6c5174d9e9149
Gabriele Ribichini <gabriele.ribichini@gmail.com>
2015-05-16 09:06:58 +0200

    added remote input for stairs light

--- node00_LOFTB1/node00_LOFTB1_PinMapping.h
+++ node00_LOFTB1/node00_LOFTB1_PinMapping.h
@@ -1,14 +1,23 @@
 
 
-// DEFINE INPUT PIs
-#define LIGHT_LOFT_1_PIN                        30
-#define LIGHT_LOFT_2_PIN                        31
-#define LIGHT_TERRACE_1_PIN                     32
-#define LIGHT_TERRACE_2_PIN                     33
-#define LIGHT_TERRACE_3_PIN                     34
-#define LIGHT_TOILET_PIN                        35
+// DEFINE INPUT PINs
+#define LIGHT_STAIRS_PIN_IN                     48
+#define LIGHT_LOFT_PIN_IN                     	49
+#define LIGHT_TERRACE_1_PIN_IN                  50
+#define LIGHT_TERRACE_2_PIN_IN                  51
+#define LIGHT_TERRACE_3_PIN_IN                  52
+#define LIGHT_TOILET_PIN_IN                     53
+
+#define TEMP_BOILER_SANITARY_PIN 	A0       // analog pin used
+#define TEMP_BOILER_HEATING_PIN 	A1       // analog pin used
+#define TEMP_BOILER_BOTTOM_PIN  	A2       // analog pin used
+#define TEMP_FLOOR_FLOW_PIN   		A3       // analog pin used
+#define TEMP_FLOOR_RETURN_PIN 		A4       // analog pin used
+#define TEMP_FANCOIL_FLOW_PIN 		A5       // analog pin used
+
 #define MAIN_3WAY_VALVE_BOILER_LIMIT_PIN		9
-#define MAIN_3WAY_VALVE_COLLECTOR_LIMIT_PIN	8
+#define MAIN_3WAY_VALVE_COLLECTOR_LIMIT_PIN		8
+
 
 // DEFINE OUTPUT PINs
 // ** DO NOT FORGET TO SET pinMode to OUTPUT **
@@ -19,6 +28,14 @@
 #define MAIN_3WAY_VALVE_COLLECTOR_PIN			27
 #define HEATING_MIX_VALVE_SWITCH_PIN			28
 #define HEATING_MIX_VALVE_DIRECTION_PIN			29
+
+#define LIGHT_LOFT_1_PIN                        30
+#define LIGHT_LOFT_2_PIN                        31
+#define LIGHT_TERRACE_1_PIN                     32
+#define LIGHT_TERRACE_2_PIN                     33
+#define LIGHT_TERRACE_3_PIN                     34
+#define LIGHT_TOILET_PIN                        35
+
 #define HEATPUMP_REMOTE_SWITCH_PIN              36
 #define HEATPUMP_CIRCULATION_PUMP_PIN   		37
 #define HEATPUMP_SANITARY_REQUEST_PIN           38
@@ -31,17 +48,4 @@
 #define ZONE_SWITCH_KITCHEN_PIN                 45
 #define ZONE_SWITCH_LIVING_PIN                  46
 #define ZONE_SWITCH_LOFT_PIN                    47
-#define LIGHT_LOFT_1_PIN_IN                     48
-#define LIGHT_LOFT_2_PIN_IN                     49
-#define LIGHT_TERRACE_1_PIN_IN                  50
-#define LIGHT_TERRACE_2_PIN_IN                  51
-#define LIGHT_TERRACE_3_PIN_IN                  52
-#define LIGHT_TOILET_PIN_IN                     53
 
-
-#define TEMP_BOILER_SANITARY_PIN 	A0       // analog pin used
-#define TEMP_BOILER_HEATING_PIN 	A1       // analog pin used
-#define TEMP_BOILER_BOTTOM_PIN  	A2       // analog pin used
-#define TEMP_FLOOR_FLOW_PIN   		A3       // analog pin used
-#define TEMP_FLOOR_RETURN_PIN 		A4       // analog pin used
-#define TEMP_FANCOIL_FLOW_PIN 		A5       // analog pin used
--- node00_LOFTB1/node00_LOFTB1_Slots.h
+++ node00_LOFTB1/node00_LOFTB1_Slots.h
@@ -1,3 +1,4 @@
+#define LIGHT_STAIRS					0
 #define LIGHT_LOFT_1            		1                       
 #define LIGHT_LOFT_2            		2
 #define LIGHT_TERRACE_1                 3
